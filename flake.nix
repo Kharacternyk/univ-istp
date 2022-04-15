f4003b96ee5b76bbaf3a180fdad0deae5273a715
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-15 21:40:07 +0300

    Switch to flake devShell with direnv.

--- flake.nix
+++ flake.nix
@@ -1,32 +1,15 @@
 {
   outputs = { self, nixpkgs }: with import nixpkgs { system = "x86_64-linux"; }; {
-    packages.x86_64-linux = {
-      python = pkgs.python310.withPackages (p: [p.psycopg2 p.django]);
-      django = pkgs.writeShellScriptBin "django" ''
-        export DJANGO_SETTINGS_MODULE=istp.settings
-        ${self.packages.x86_64-linux.python}/bin/python -m django "$@"
+    devShell.x86_64-linux = pkgs.mkShell {
+      packages = [
+        pkgs.postgresql_14
+        (pkgs.python310.withPackages (p: [p.psycopg2 p.django]))
+      ];
+      shellHook = ''
+        export PGDATA="$(realpath .)/.pgdata"
+        export PGHOST="$PGDATA"
       '';
-      devServer = pkgs.writeShellScriptBin "server" ''
-        ${pkgs.postgresql_14}/bin/postgres -D .pgdata &
-        ${self.packages.x86_64-linux.django}/bin/django runserver
-      '';
-      psql = pkgs.writeShellScriptBin "psql" ''
-        ${pkgs.postgresql_14}/bin/psql -h "$PWD/.pgdata" -d bgclub
-      '';
-    };
-    apps.x86_64-linux = {
-      django = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.django}/bin/django";
-      };
-      devServer = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.devServer}/bin/server";
-      };
-      psql = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.psql}/bin/psql";
-      };
+      PGDATABASE = "bgclub";
     };
   };
 }
