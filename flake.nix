63445df5e9d04ad48759d19643c0764ec71620c8
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-23 19:46:18 +0300

    Avoid hardcoding the architecture.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
-  outputs = { self, nixpkgs }: with import nixpkgs { system = "x86_64-linux"; }; {
-    devShell.x86_64-linux = pkgs.mkShell {
-      packages = [
-        pkgs.postgresql_14
-        (pkgs.python310.withPackages (p: [p.psycopg2 p.django]))
-      ];
-    };
-  };
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; }; {
+      devShell = pkgs.mkShell {
+        packages = [
+          pkgs.postgresql_14
+          (pkgs.python310.withPackages (p: [ p.psycopg2 p.django ]))
+        ];
+      };
+    }
+  );
 }
