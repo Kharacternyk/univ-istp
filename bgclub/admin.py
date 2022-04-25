6563a441b4fabc3228dbc33893c0f16a312cd65f
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-25 20:05:41 +0300

    Translate to Ukrainian.

--- bgclub/admin.py
+++ bgclub/admin.py
@@ -11,53 +11,74 @@ class PlaySessionForm(ModelForm):
         start_time = self.cleaned_data.get("start_time")
         end_time = self.cleaned_data.get("end_time")
         if start_time and end_time and end_time < start_time:
-            raise ValidationError({"end_time": "Must be after the start time."})
+            raise ValidationError(
+                {"end_time": "Сеанс не може закінчитись раніше за час початку."}
+            )
 
 
 class ClubMemberForm(ModelForm):
     def clean(self):
         phone = self.cleaned_data.get("phone")
         if phone and phone < 1e6:
-            raise ValidationError({"phone": "Must have at least 6 digits."})
+            raise ValidationError({"phone": "Телефон має містити не менше шести цифр."})
 
 
 class GameLocalizationForm(ModelForm):
     def clean(self):
         barcode = self.cleaned_data.get("barcode")
-
         if not barcode:
             return
-
-        if barcode < 0:
-            raise ValidationError({"barcode": "Must be a positive integer."})
-
         digits = int(log10(barcode))
         if not 12 <= digits <= 13:
             raise ValidationError(
-                {"barcode": f"Must have either 12 or 13 digits, got {digits}."}
+                {"barcode": f"Штрих-код має мати 12 або 13 цифр, отримано {digits}."}
             )
 
 
+class AuthorshipForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["author"].label = "автор"
+        self.fields["game"].label = "гра"
+
+
+class PlayerForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["play_session"].label = "сеанс"
+        self.fields["club_member"].label = "гравець"
+
+
+class ItemForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["play_session"].label = "сеанс"
+        self.fields["game"].label = "гра"
+
+
 class InlineAuthorship(admin.TabularInline):
+    form = AuthorshipForm
     model = models.Authorship
     extra = 1
 
 
-class InlinePlayers(admin.TabularInline):
-    model = models.Players
+class InlinePlayer(admin.TabularInline):
+    form = PlayerForm
+    model = models.Player
     extra = 2
 
 
-class InlineItems(admin.TabularInline):
-    model = models.PlaySessionItems
+class InlineItem(admin.TabularInline):
+    form = ItemForm
+    model = models.PlaySessionItem
     extra = 1
 
 
 @admin.register(
-    models.Categories,
-    models.Countries,
-    models.Languages,
-    models.Publishers,
+    models.Category,
+    models.Country,
+    models.Language,
+    models.Publisher,
 )
 class NarrowTextAdmin(admin.ModelAdmin):
     formfield_overrides = {
@@ -65,22 +86,22 @@ class NarrowTextAdmin(admin.ModelAdmin):
     }
 
 
-@admin.register(models.Authors, models.Games)
+@admin.register(models.Author, models.Game)
 class AuthorshipAdmin(NarrowTextAdmin):
     inlines = tuple([InlineAuthorship])
 
 
-@admin.register(models.PlaySessions)
+@admin.register(models.PlaySession)
 class PlaySessionsAdmin(NarrowTextAdmin):
     form = PlaySessionForm
-    inlines = (InlineItems, InlinePlayers)
+    inlines = (InlineItem, InlinePlayer)
 
 
-@admin.register(models.ClubMembers)
+@admin.register(models.ClubMember)
 class ClubMembersAdmin(NarrowTextAdmin):
     form = ClubMemberForm
 
 
-@admin.register(models.GameLocalizations)
+@admin.register(models.GameLocalization)
 class GameLocalizationsAdmin(NarrowTextAdmin):
     form = GameLocalizationForm
--- bgclub/models.py
+++ bgclub/models.py
@@ -1,149 +1,202 @@
 from django.db import models
 
 
-class Authors(models.Model):
-    name = models.TextField(unique=True)
-    country = models.ForeignKey("Countries", models.CASCADE, blank=True, null=True)
-    games = models.ManyToManyField("Games", through="Authorship")
+class Author(models.Model):
+    name = models.TextField("ім'я", unique=True)
+    country = models.ForeignKey(
+        "Country", models.CASCADE, blank=True, null=True, verbose_name="країна"
+    )
+    games = models.ManyToManyField("Game", through="Authorship", verbose_name="ігри")
 
     def __str__(self):
         return f"{self.name} [{self.country}]"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "authors"
+        verbose_name_plural = "автори"
+        verbose_name = "автор"
+        db_table = "authors"
 
 
-class Categories(models.Model):
-    name = models.TextField(unique=True)
+class Category(models.Model):
+    name = models.TextField("ім'я", unique=True)
 
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "categories"
+        verbose_name_plural = "категорії"
+        verbose_name = "категорія"
+        db_table = "categories"
 
 
-class ClubMembers(models.Model):
-    phone = models.BigIntegerField(unique=True)
-    name = models.TextField(blank=True, null=True)
-    play_sessions = models.ManyToManyField("PlaySessions", through="Players")
+class ClubMember(models.Model):
+    phone = models.BigIntegerField("телефон", unique=True)
+    name = models.TextField("ім'я", blank=True, null=True)
+    play_sessions = models.ManyToManyField(
+        "PlaySession", through="Player", verbose_name="ігрові сеанси"
+    )
 
     def __str__(self):
         return f"{self.name} [{self.phone}]"
 
     class Meta:
         managed = False
-        verbose_name_plural = "club members"
+        verbose_name_plural = "гравці клубу"
+        verbose_name = "гравець клубу"
         db_table = "club_members"
 
 
-class Countries(models.Model):
-    name = models.TextField(unique=True)
+class Country(models.Model):
+    name = models.TextField("ім'я", unique=True)
 
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "countries"
+        verbose_name_plural = "країни"
+        verbose_name = "країна"
+        db_table = "countries"
 
 
-class GameLocalizations(models.Model):
-    barcode = models.BigIntegerField(primary_key=True)
-    game = models.ForeignKey("Games", models.CASCADE)
-    name = models.TextField()
-    publishing_date = models.DateField(blank=True, null=True)
-    in_catalog_since_date = models.DateField(blank=True, null=True)
-    in_catalog_count = models.IntegerField()
-    language = models.ForeignKey("Languages", models.CASCADE, blank=True, null=True)
-    publisher = models.ForeignKey("Publishers", models.CASCADE, blank=True, null=True)
-    play_sessions = models.ManyToManyField("PlaySessions", through="PlaySessionItems")
+class GameLocalization(models.Model):
+    barcode = models.BigIntegerField("штрих-код", primary_key=True)
+    game = models.ForeignKey("Game", models.CASCADE, verbose_name="гра")
+    name = models.TextField("ім'я")
+    publishing_date = models.DateField("дата видання", blank=True, null=True)
+    in_catalog_since_date = models.DateField(
+        "дата появи у каталозі", blank=True, null=True
+    )
+    in_catalog_count = models.IntegerField("наявна кількість у каталозі")
+    language = models.ForeignKey(
+        "Language", models.CASCADE, blank=True, null=True, verbose_name="мова"
+    )
+    publisher = models.ForeignKey(
+        "Publisher", models.CASCADE, blank=True, null=True, verbose_name="видавець"
+    )
+    play_sessions = models.ManyToManyField(
+        "PlaySession", through="PlaySessionItem", verbose_name="ігрові сеанси"
+    )
 
     def __str__(self):
         return f"{self.name} [{self.barcode}]"
 
     class Meta:
         managed = False
-        verbose_name_plural = "game localization"
+        verbose_name_plural = "локалізації ігор"
+        verbose_name = "локалізація гри"
         db_table = "game_localizations"
 
 
-class Games(models.Model):
-    name = models.TextField()
-    playtime = models.IntegerField(blank=True, null=True)
-    min_players = models.IntegerField(blank=True, null=True)
-    max_players = models.IntegerField(blank=True, null=True)
-    category = models.ForeignKey(Categories, models.CASCADE, blank=True, null=True)
+class Game(models.Model):
+    name = models.TextField("ім'я")
+    playtime = models.IntegerField("орієнтовний час гри", blank=True, null=True)
+    min_players = models.IntegerField(
+        "мінімальна кількість гравців", blank=True, null=True
+    )
+    max_players = models.IntegerField(
+        "максимальна кількість гравців", blank=True, null=True
+    )
+    category = models.ForeignKey(
+        Category, models.CASCADE, blank=True, null=True, verbose_name="категорія"
+    )
+    authors = models.ManyToManyField(
+        "Author", through="Authorship", verbose_name="автори"
+    )
 
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "games"
+        verbose_name_plural = "ігри"
+        verbose_name = "гра"
+        db_table = "games"
 
 
-class Languages(models.Model):
-    name = models.TextField(unique=True)
+class Language(models.Model):
+    name = models.TextField("ім'я", unique=True)
 
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "languages"
+        verbose_name_plural = "мови"
+        verbose_name = "мова"
+        db_table = "languages"
 
 
-class PlaySessions(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+class PlaySession(models.Model):
+    start_time = models.DateTimeField("час початку")
+    end_time = models.DateTimeField("час кінця")
 
     def __str__(self):
-        return f"{self.id} [{self.start_time} - {self.end_time}]"
+        return f"{self.id} [{self.start_time} — {self.end_time}]"
 
     class Meta:
         managed = False
-        verbose_name_plural = "play sessions"
+        verbose_name_plural = "ігрові сеанси"
+        verbose_name = "ігровий сеанс"
         db_table = "play_sessions"
 
 
-class Publishers(models.Model):
-    name = models.TextField(unique=True)
-    country = models.ForeignKey(Countries, models.CASCADE, blank=True, null=True)
+class Publisher(models.Model):
+    name = models.TextField("ім'я", unique=True)
+    country = models.ForeignKey(
+        Country, models.CASCADE, blank=True, null=True, verbose_name="країна"
+    )
 
     def __str__(self):
         return f"{self.name} [{self.country}]"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "publishers"
+        verbose_name_plural = "видавці"
+        verbose_name = "видавець"
+        db_table = "publishers"
 
 
 class Authorship(models.Model):
-    author = models.ForeignKey(Authors, models.CASCADE)
-    game = models.ForeignKey("Games", models.CASCADE)
+    author = models.ForeignKey(Author, models.CASCADE)
+    game = models.ForeignKey("Game", models.CASCADE)
+
+    def __str__(self):
+        return "Гра/Сеанс"
 
     class Meta:
         managed = False
+        verbose_name = "авторство"
+        verbose_name_plural = "авторство"
         db_table = "authorship"
 
 
-class PlaySessionItems(models.Model):
-    play_session = models.ForeignKey("PlaySessions", models.CASCADE)
-    game = models.ForeignKey(GameLocalizations, models.CASCADE, db_column="barcode")
-    count = models.IntegerField(default=1)
+class PlaySessionItem(models.Model):
+    play_session = models.ForeignKey("PlaySession", models.CASCADE)
+    game = models.ForeignKey(GameLocalization, models.CASCADE, db_column="barcode")
+    count = models.PositiveIntegerField("кількість", default=1)
+
+    def __str__(self):
+        return "Зв'язок"
 
     class Meta:
         managed = False
+        verbose_name = "гра в сеансі"
+        verbose_name_plural = "ігри в сеансі"
         db_table = "play_session_items"
 
 
-class Players(models.Model):
-    club_member = models.ForeignKey(ClubMembers, models.CASCADE)
-    play_session = models.ForeignKey(PlaySessions, models.CASCADE)
+class Player(models.Model):
+    club_member = models.ForeignKey(ClubMember, models.CASCADE)
+    play_session = models.ForeignKey(PlaySession, models.CASCADE)
+
+    def __str__(self):
+        return "Зв'язок"
 
     class Meta:
         managed = False
-        verbose_name_plural = db_table = "players"
+        verbose_name = "гравець у сеансі"
+        verbose_name_plural = "гравці в сеансі"
+        db_table = "players"
