7c6dc9447fa0ddeb323400afe91b1318d9cec096
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-25 13:44:30 +0300

    Avoid unbound function calls in flake.nix.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     with import nixpkgs { inherit system; }; {
-      devShell = pkgs.mkShell {
+      devShell = with {
+        python = pkgs.python310.withPackages (p: [ p.psycopg2 p.django ]);
+      }; pkgs.mkShell {
         packages = [
+          python
           pkgs.postgresql_14
-          (pkgs.python310.withPackages (p: [ p.psycopg2 p.django ]))
         ];
       };
     }
