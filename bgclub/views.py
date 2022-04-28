bb3857fc6b4c79f8699be2af5319cda51ff804e4
Nazar Vinnichuk <nazar@vinnich.uk>
2022-04-28 21:40:48 +0300

    Add a basic index view.

--- bgclub/views.py
+++ bgclub/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from bgclub.models import GameLocalization
 
-# Create your views here.
+
+class IndexView(ListView):
+    template_name = "index.html"
+    model = GameLocalization
