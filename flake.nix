f694a6f1e3b047bff30d80581ce8c98b277ff5e0
Nazar Vinnichuk <nazar@vinnich.uk>
2022-05-04 23:15:37 +0300

    Use django_bootstrap5.

--- flake.nix
+++ flake.nix
@@ -7,11 +7,26 @@
 
   outputs = { self, nixpkgs, utils, pre-commit }: utils.lib.eachDefaultSystem (system:
     with import nixpkgs { inherit system; }; {
-      devShell = with {
+      devShell = with rec {
+        django-bootstrap = python310Packages.buildPythonPackage rec {
+          pname = "django-bootstrap5";
+          version = "21.3";
+          src = python310Packages.fetchPypi {
+            inherit pname version;
+            format = "setuptools";
+            sha256 = "NQhjQYgXgKRLLiclWJT2Ap/F73XloOyOvYL0elGE+nM=";
+          };
+          propagatedBuildInputs = [
+            python310Packages.django
+            python310Packages.beautifulsoup4
+          ];
+          doCheck = false;
+        };
         python = python310.withPackages (pythonPackages: with pythonPackages; [
           psycopg2
           django
           django-tables2
+          django-bootstrap
         ]);
       }; mkShell {
         packages = [
--- istp/settings.py
+++ istp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "bgclub.apps.BgclubConfig",
     "django_tables2",
+    "django_bootstrap5",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
